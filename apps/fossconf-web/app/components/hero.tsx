e532bb041395c5e252e84a49ee8acb673b6b64f2
vivekpal1 <vivekpal@duck.com>
2023-10-11 00:44:17 +0530

    button

--- apps/fossconf-web/app/components/hero.tsx
+++ apps/fossconf-web/app/components/hero.tsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import Link from 'next/link';
-import Logo from './logo';
+import React from "react";
+import Logo from "./logo";
+import Button from "./button";
 
 export default function Hero() {
-    return (
-        <div className="relative w-screen h-screen bg-black">
-            <video
-                autoPlay
-                loop
-                muted
-                className="absolute top-0 left-0 w-full h-full object-cover"
-                style={{ filter: 'blur(4px)' }}
-            >
-                <source src="/hero.mp4" type="video/mp4" />
-            </video>
+  return (
+    <div className="flex w-screen h-screen bg-black">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        style={{ filter: "blur(4px)" }}
+      >
+        <source src="/hero.mp4" type="video/mp4" />
+      </video>
 
-            <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-start p-16">
-                <Logo/>
-                <p className="text-white mb-8">First Annual Conference of FOSSCU</p>
-                <div className="flex space-x-4">
-                    <Link href="#">
-                        <div className="cursor-pointer text-white underline">Button 1</div>
-                    </Link>
-                    <Link href="#">
-                        <div className="cursor-pointer text-white underline">Button 2</div>
-                    </Link>
-                </div>
-            </div>
+      <div className="absolute top-0 left-0 ml-16 mt-16">
+        <Logo />
+      </div>
+
+      <div className="absolute top-1/4 left-16">
+        <p className="text-yellow-200 font-bold text-6xl mb-8">
+          First Annual Conference of FOSSCU
+        </p>
+      </div>
+
+      <div className="absolute bottom-16 left-16 flex space-x-4">
+        <div className="absolute bottom-16 left-16 flex space-x-4">
+          <Button text="Tickets" href="#" />
+          <Button text="Get Involved" href="#" />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- apps/fossconf-web/app/components/logo.tsx
+++ apps/fossconf-web/app/components/logo.tsx
@@ -3,8 +3,8 @@ import React from "react";
 function Logo() {
   return (
     <svg
-      width="115"
-      height="16"
+      width="288"
+      height="40"
       viewBox="0 0 115 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
