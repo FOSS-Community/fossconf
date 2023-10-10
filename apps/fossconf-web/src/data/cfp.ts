9c49f4276656c528cba4698ced753a20e7e42a4b
vivekpal1 <vivekpal@duck.com>
2023-10-11 04:58:37 +0530

    form

--- apps/fossconf-web/src/data/cfp.ts
+++ apps/fossconf-web/src/data/cfp.ts
@@ -1,3 +1,3 @@
 export const deadline = new Date('2023-11-26');
-export const link = 'https://conference.fosscu.org/';
+export const link = 'https://tally.so/r/m6KRl5';
 export const name = 'Apply now!';
