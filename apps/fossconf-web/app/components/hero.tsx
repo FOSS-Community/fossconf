c0f7f2f644421ca4e9aa331a56008aeebcda6544
vivekpal1 <vivekpal@duck.com>
2023-10-10 22:35:17 +0530

    logo

--- apps/fossconf-web/app/components/hero.tsx
+++ apps/fossconf-web/app/components/hero.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
+import Logo from './logo';
 
 export default function Hero() {
     return (
-        <div className="relative w-full h-screen bg-black">
+        <div className="relative w-screen h-screen bg-black">
             <video
                 autoPlay
                 loop
                 muted
-                className="absolute w-full h-full object-cover"
-                style={{ filter: 'blur(8px)' }}
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                style={{ filter: 'blur(4px)' }}
             >
                 <source src="/hero.mp4" type="video/mp4" />
             </video>
 
             <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-start p-16">
-                <h3 className="text-white mb-4">FOSSConf 2023</h3>
+                <Logo/>
                 <p className="text-white mb-8">First Annual Conference of FOSSCU</p>
                 <div className="flex space-x-4">
                     <Link href="#">
