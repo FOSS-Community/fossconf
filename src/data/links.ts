f0610a3d38f16aac936d7c658636fc77ffb02eb0
vivekpal1 <vivekpal@duck.com>
2023-10-18 12:10:36 +0530

    other changes

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,6 +1,6 @@
 export const volunteerLink = '/discord';
 export const discordLink = '/discord';
-export const discordInviteUrl = 'https://discord.gg/26ydZDKhVp';
+export const discordInviteUrl = 'https://dub.sh/fosscu-discord';
 export const ticketLink =
   '/';
 export const venueLink = 'https://maps.app.goo.gl/2ybitPxHdJJx62yo7';
