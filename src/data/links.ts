3ebabb340d187e327cbb6ce8d1b9081bcd4b6bf3
vivekpal1 <vivekpal@duck.com>
2023-10-18 12:30:30 +0530

    -

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,7 +1,7 @@
 export const volunteerLink = '/discord';
 export const discordLink = '/discord';
 export const discordInviteUrl = 'https://dub.sh/fosscu-discord';
-export const ticketLink =
-  '/';
+export const ticketLink = '/';
 export const venueLink = 'https://maps.app.goo.gl/2ybitPxHdJJx62yo7';
 export const venueName = 'Fosscu';
+export const hackathonLink = '/';
