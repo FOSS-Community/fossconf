8d515f594f55da592fee8a83bef4ad54ece9dedb
vivekpal1 <vivekpal@duck.com>
2023-10-20 00:32:32 +0530

    links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,15 +1,7 @@
 export const volunteerLink = 'https://airtable.com/appyqESu1jTLyRj5b/shrADLMpduaaLPS9R';
 export const discordLink = '/discord';
-<<<<<<< HEAD
 export const discordInviteUrl = 'https://dub.sh/fosscu-discord';
 export const ticketLink = '/';
 export const venueLink = 'https://maps.app.goo.gl/gx74EhJNz71mdwmS7';
 export const venueName = 'Announcing Soon';
 export const hackathonLink = '/';
-=======
-export const discordInviteUrl = 'https://discord.gg/zh2UmCxjVj';
-export const ticketLink =
-  '/';
-export const venueLink = 'https://maps.app.goo.gl/2ybitPxHdJJx62yo7';
-export const venueName = 'Fosscu';
->>>>>>> 00864ba (FAQ)
