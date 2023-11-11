040604a2d4eea108660b7b884ebe182110262248
vivek <31vivekpal@gmail.com>
2023-11-11 11:58:30 +0530

    venue link

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,6 +2,6 @@ export const volunteerLink = 'https://airtable.com/appyqESu1jTLyRj5b/shrADLMpdua
 export const discordLink = '/discord';
 export const discordInviteUrl = 'https://dub.sh/fosscu-discord';
 export const ticketLink = '/tickets';
-export const venueLink = '/';
+export const venueLink = 'https://maps.app.goo.gl/iCjzVf4SjrCrz4RS6';
 export const gettingThereLink ='/venue';
 export const venueName = 'IIIT Delhi, New Delhi';
