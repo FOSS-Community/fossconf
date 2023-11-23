51b9281869ca5f9d6b4de561039ebf40e9d508c5
vivekpal1 <31vivekpal@gmail.com>
2023-11-24 01:51:18 +0530

    calenderLink

--- src/data/links.ts
+++ src/data/links.ts
@@ -5,3 +5,5 @@ export const ticketLink = '/tickets';
 export const venueLink = 'https://maps.app.goo.gl/iCjzVf4SjrCrz4RS6';
 export const gettingThereLink ='/venue';
 export const venueName = 'IIIT Delhi, New Delhi';
+export const calenderLink = 'https://calendar.google.com/calendar/u/0?cid=ZTlhMDI2Yjg1OWI1NWMzNTY3OTdlMTMzMzAxYzE1NDc2ZjNhOWI3YzJlOGUwOGVhNmE2ZDczOTVlZjYyYjA5N0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t';
+
