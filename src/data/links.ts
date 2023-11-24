62642275d8984831765941595cd5be510a6b3a2f
codecShivam <codecshivam@gmail.com>
2023-11-24 23:12:20 +0530

    fixed venueLink

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,7 @@ export const volunteerLink = 'https://airtable.com/appyqESu1jTLyRj5b/shrADLMpdua
 export const discordLink = '/discord';
 export const discordInviteUrl = 'https://dub.sh/fosscu-discord';
 export const ticketLink = '/tickets';
-export const venueLink = 'https://maps.app.goo.gl/iCjzVf4SjrCrz4RS6';
+export const venueLink = 'https://maps.app.goo.gl/KoYbmuzhRMX2d5Ci8';
 export const gettingThereLink ='/venue';
 export const venueName = 'IIIT Delhi, New Delhi';
 export const calenderLink = 'https://calendar.google.com/calendar/u/0?cid=ZTlhMDI2Yjg1OWI1NWMzNTY3OTdlMTMzMzAxYzE1NDc2ZjNhOWI3YzJlOGUwOGVhNmE2ZDczOTVlZjYyYjA5N0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t';
